3e08ec1ad370ba9cc76b85a43126d67ce0eb6ec1
Igrok-1 <yaroslavmilushkin2010@gmail.com>
2026-04-21 10:29:45 +0300

    Update header.jsx

---
+++ components/header.jsx
@@ -0,0 +1,19 @@
+export default function header(){
+  return(
+    <header>
+      <nav>
+        <button className="{styles.main}">Главная</button>
+        <button className="{styles.profile}">Профиль</button>
+        <ul className="{styles.profileUl}">
+          <li className="{styles.setings}">Настройки</li>
+          <li className="{styles.quit}">Выйти</li>
+        </ul>
+        <button className="{styles.problems}">Мои задачи</button>
+        <button className="{styles.calendar}">Календарь</button>
+        <button className="{styles.info}">О нас</button>
+        <img src="" alt=""/>                                
+      </nav>
+    </header>
+
+  
+}
